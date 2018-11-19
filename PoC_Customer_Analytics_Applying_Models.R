913f42f9edd491ec466384f2d249294c831ee0e0
egocustos <ego_custos@hotmail.com>
2018-11-19 17:02:07 -0300

    Update PoC_Customer_Analytics_Applying_Models.R

--- PoC_Customer_Analytics_Applying_Models.R
+++ PoC_Customer_Analytics_Applying_Models.R
@@ -6,63 +6,124 @@ orange_test <-read.csv("ORANGE_TEST_DATA.csv")
 objectives <- read.csv("KDD_CUP_2009_OBJECTIVE_COLUMNS_LABELS.csv")
 
 # Encoding the target feature as factor (In this case, Churn)
-Churn <- factor(objectives$Churn, levels = c(-1, 1))
-
-dataset <- cbind(orange_train,Churn)
-
-#View(dataset$Churn)
-
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Churn, SplitRatio = 0.75)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+objectives$Churn[objectives$Churn==-1]<-0
+Churn <- factor(objectives$Churn, levels = c(0, 1))
 
 #JC: Logistic, Naive Bayes, Decision Tree, Random Forest.
 #Cristian: K-NN, SVM, Kernel SVM
 
+# Logistic Regression -----------------------------------------------------
+
+#It is required to omit the columns which have 0 variance
+NoVar <- function(dat) {
+  out <- lapply(dat, function(x) length(unique(x)))
+  want <- which(!out > 1)
+  unlist(want)
+}
+
+train_log <- subset(orange_train, select = -NoVar(orange_train))
+
+#which(names(train)=="Var190")
+#ncol(train[1:160])
+dataset_log <- cbind(train_log[1:160],Churn)
+
+#Replacing missing values with the respective mean of each colum
+library(zoo)
+df <- dataset_log
+df[] <- lapply(df, na.aggregate)
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(321)
+split_log = sample.split(df$Churn, SplitRatio = 0.75)
+training_set_log = subset(df, split_log == TRUE)
+test_set_log = subset(df, split == FALSE)
+
+
 # Fitting Logistic Regression to the Training set
+
 classifier_log = glm(formula = Churn ~ .,
                  family = binomial,
                  data = training_set_log)
 
 # Predicting the Test set results
-y_pred_log = predict(classifier_log, newdata = test_set[-3])
+prob_pred_log = predict(classifier_log, type = 'response', newdata = test_set_log[-161])
+y_pred_log = ifelse(prob_pred_log > 0.5, 1, 0)
 
 
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred)
+cm_log = table(test_set[, 161], y_pred_log > 0.5)
+#Results: 
+# True Negatives: 11573/11582 (99.92%)
+# True Positives: 1/918 (0.11%)    
 
-# Visualising the Training set results
-library(ElemStatLearn)
-set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3],
-     main = 'Classifier (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
-# Visualising the Test set results
-library(ElemStatLearn)
-set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3], main = 'Classifier (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+# Performing the splitting to the rough dataset-------------------------------------------------------------
 
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+train <- cbind(orange_train, Churn)
+
+library(caTools)
+set.seed(123)
+split = sample.split(train$Churn, SplitRatio = 0.75)
+training_set = subset(train, split == TRUE)
+test_set = subset(train, split == FALSE)
+
+
+# Naive Bayes -------------------------------------------------------------
+
+
+# Fitting NB to the Training set
+#install.packages('e1071')
+library(e1071)
+classifier_nb = naiveBayes(x = training_set[-231],
+                        y = training_set$Churn)
+
+# Predicting the Test set results
+y_pred_nb = predict(classifier_nb, newdata = test_set[-231])
+
+# Making the Confusion Matrix
+cm_nb = table(test_set[, 231], y_pred_nb)
+#Results: 
+# True Negatives: 2304/11582 (19.89%)
+# True Positives: 822/918 (89.54%)
+
+
+# Decision Tree -----------------------------------------------------------
+
+# Fitting Decision Tree Classification to the Training set
+#install.packages('rpart')
+library(rpart)
+classifier_dt = rpart(formula = Churn ~ .,
+                   data = training_set)
+
+# Predicting the Test set results
+y_pred_dt = predict(classifier_dt, newdata = test_set[-231], type = 'class')
+
+# Making the Confusion Matrix
+cm_dt = table(test_set[, 231], y_pred_dt)
+#Results: 
+# True Negatives: 11164/11582 (96.39%)
+# True Positives: 51/918 (5.56%)
+
+
+# Random Forest -----------------------------------------------------------
+
+#RF does not allow NA's in predictors
+training_set[!complete.cases(training_set),]
+
+# Fitting Random Forest Classification to the Training set
+# install.packages('randomForest')
+library(randomForest)
+set.seed(123)
+classifier_rf = randomForest(x = training_set[-231],
+                          y = training_set$Churn,
+                          ntree = 500)
+
+# Predicting the Test set results
+y_pred_rf = predict(classifier_rf, newdata = test_set[-231])
+
+# Making the Confusion Matrix
+cm_rf = table(test_set[, 231], y_pred_rf)
